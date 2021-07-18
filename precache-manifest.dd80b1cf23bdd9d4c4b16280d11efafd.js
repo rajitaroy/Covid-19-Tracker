6c4e4c139055ae7d7edead37d968c07f833d9a90
Rajita Roy <royrajita6@gmail.com>
2021-07-18 22:59:15 +0530

    Updates

---
+++ precache-manifest.dd80b1cf23bdd9d4c4b16280d11efafd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6a2276b3ef47d1177625898b3566f4",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "85c2c4f8f1139813ab81",
+    "url": "/Covid-19-Tracker/static/css/main.a494f472.chunk.css"
+  },
+  {
+    "revision": "feb4c284ff3f20da9e1f",
+    "url": "/Covid-19-Tracker/static/js/2.47a2265b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.47a2265b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c2c4f8f1139813ab81",
+    "url": "/Covid-19-Tracker/static/js/main.9d507f02.chunk.js"
+  },
+  {
+    "revision": "46a1a27f48aa23b9959c",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.9af3beb3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
